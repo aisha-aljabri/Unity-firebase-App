be149157c00e6c1c055365536106da2b6f5fe5d4
Aisha AL-Jabri <46584527+aisha-aljabri@users.noreply.github.com>
2020-06-03 20:58:42 +0300

    Add files via upload

---
+++ home.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class home : MonoBehaviour
+{
+    DatabaseReference childs;
+    public InputField name;
+    public InputField password;
+
+ public void Save()
+    {
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://graduation-project-11.firebaseio.com/");
+        childs = FirebaseDatabase.DefaultInstance.RootReference;
+        
+        childs.Child(name.text).Child("password").SetValueAsync(password.text); 
+        
+        Text n = GameObject.Find("Canvas/Panel/alert").GetComponent<Text>();
+        n.text="Password saved";
+    }
+
+
+    public void Retrieve(){
+       FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://graduation-project-11.firebaseio.com/");
+        childs = FirebaseDatabase.DefaultInstance.RootReference;
+
+      FirebaseDatabase.DefaultInstance.GetReference(name.text).ValueChanged += FirebaseSaveLoadScript_ValueChanged;
+  }
+
+  private void FirebaseSaveLoadScript_ValueChanged(object sender, ValueChangedEventArgs e){
+      
+        
+        
+        Text n = GameObject.Find("Canvas/Panel/alert").GetComponent<Text>();
+        n.text = e.Snapshot.Child("password").GetValue(true).ToString();
+
+  }
+
+  public void GotoHome()
+    {SceneManager.LoadScene("home"); }
+
+
+public void GotoRetrieve()
+    {SceneManager.LoadScene("retrieve"); }
+
+
+public void GotoSave()
+    {SceneManager.LoadScene("save"); }
+
+
+
+
+}
